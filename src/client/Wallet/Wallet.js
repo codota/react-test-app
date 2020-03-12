1a30ec93a245a38de3ecaac99000dcd38dcc151b
Amir Tuval <amirtu@codota.com>
2020-03-12 15:43:42 +0200

    fix withdraw and deposit buttons and general layout

--- src/client/Wallet/Wallet.js
+++ src/client/Wallet/Wallet.js
@@ -1,13 +1,14 @@
 import React from "react";
 import './wallet.css';
+import autobind from "autobind-decorator";
+
 export default class Wallet extends React.Component {
-    state = { balance: null };
+  state = { balance: null, amount: null };
 
   componentDidMount() {
-    // this.setState({ balance:10 });
     fetch('/api/wallet/').then(response => response.json()).then(({balance}) => this.setState({ balance }));
-    
   }
+
   render() {
     return (
       <div className='wallet'>
@@ -29,18 +30,25 @@ export default class Wallet extends React.Component {
         </label>
 
         <div className='wallet-action'>
-          <button className='wallet-deposit btn btn-primary'>+</button>
+          <button disabled={!this.state.amount} className='wallet-deposit btn btn-primary'>Deposit</button>
 
             <input
               type='number'
               className='wallet-amount'
               name='amount'
               id='wallet-amount'
+              value={this.state.amount}
+              onChange={this.walletAmountChanged}
             />
 
-          <button className='wallet-withdraw btn btn-primary'>-</button>
+          <button disabled={!this.state.amount} className='wallet-withdraw btn btn-primary'>Withdraw</button>
         </div>
       </div>
     );
+  }
+
+  @autobind
+  walletAmountChanged(e) {
+    this.setState({ amount: e.target.value });
   }
 }
